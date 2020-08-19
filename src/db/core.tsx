5e2e40c1d0e58ed6505700f8f0d789132ce617ab
fatz <github@fatz.tw>
2020-08-19 21:50:25 +0800

    add segment initialization

--- src/db/core.tsx
+++ src/db/core.tsx
@@ -1,25 +1,34 @@
 import * as SQLite from 'expo-sqlite';
 
-type Tables = 'date' | 'categories';
+type Table = 'date' | 'category' | 'segment';
 
 export const db = SQLite.openDatabase('fomodoro');
 
-export const createIfNotExist = (tableName: Tables) => {
+export const createIfNotExist = (tableName: Table) => {
   db.transaction(tx => {
     let sqlStatement = '';
-    if (tableName == 'date') {
+    if (tableName === 'date') {
       sqlStatement = `
       CREATE TABLE IF NOT EXISTS ${tableName} (
         id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
         date DATE NOT NULL
       )`;
-    } else if (tableName = 'categories') {
+    } else if (tableName === 'category') {
       sqlStatement = `
       CREATE TABLE IF NOT EXISTS ${tableName} (
         id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
         name VARCHAR(30) NOT NULL
       )`;
+    } else if (tableName === 'segment') {
+      sqlStatement = `
+      CREATE TABLE IF NOT EXISTS ${tableName} (
+        id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+        date_id INTERGER NOT NULL,
+        category_id INTERGER NOT NULL,
+        start DATETIME
+      )`;
     }
+
     tx.executeSql(sqlStatement)
   }, (e) => {
     console.log(e)
---
+++ src/db/segment.tsx
@@ -0,0 +1,47 @@
+import { SQLResultSet } from 'expo-sqlite';
+import { db, createIfNotExist } from './core';
+import { DateScheme } from './date';
+import { CategoryScheme } from './category';
+
+export interface SegmentScheme {
+  id: number;
+  date_id: number;
+  category_id: number;
+  start: string; // DateTime
+}
+
+const tableName = 'segment';
+
+export const initialize = () => {
+  createIfNotExist(tableName);
+}
+
+const insert = (date_id: DateScheme['id'], category_id: CategoryScheme['id'], start: string) => {
+  return new Promise((resolve, reject) => {
+    db.transaction(tx => {
+      tx.executeSql(`
+          INSERT INTO ${tableName} (date_id, category_id, start) VALUES (?, ?, ?)
+        `, [date_id, category_id, start], (tx, result) => {
+        resolve(result);
+      }, (tx, e) => {
+        reject(e);
+        return false;
+      })
+    })
+  })
+}
+
+
+// export const someByDate = (): Promise<SQLResultSet> => {
+//   return new Promise((resolve, reject) => {
+//     db.transaction(tx => {
+//       tx.executeSql(`
+//         SELECT * FROM ${tableName}`, [], (tx, result) => {
+//         resolve(result)
+//       }, (tx, e) => {
+//         reject(e);
+//         return false;
+//       })
+//     })
+//   })
+// }
